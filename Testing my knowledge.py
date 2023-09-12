f6075ce751fdc68b4793a7f2c772037279444eed
DanielDanielDanielandmoreDaniel <138410451+DanielDanielDanielandmoreDaniel@users.noreply.github.com>
2023-09-12 14:30:04 +1200

    Update Testing my knowledge.py
    Added comments

--- Testing my knowledge.py
+++ Testing my knowledge.py
@@ -1,6 +1,8 @@
 import random
 
-
+# This was the second challenge, i had to add 2 numbers
+# I decided to make it a bit more advanced, by getting the user to input the numbers
+# I even added a confirmation option if they wanted to change their numbers
 def calc():
     num1 = int(input("Pick a number >> "))
     num2 = int(input("Pick another number >> "))
@@ -11,6 +13,8 @@ def calc():
         calc()
 
 
+# This function simply generates a random number and prints it
+# Again i make ti a big more complicated by asking if they wanted another one
 def rand():
     randint = random.randint(0, 10)
     print(f"My random number is {randint}")
@@ -21,21 +25,35 @@ def rand():
         return randint
 
 
+# The first challenge was to print "Hello world"
 print("Part 1: Hello World!")
 
+# This is the second challenge which is all done in a function
 print("\nPart 2:")
 calc()
 
+# The third challenge was to find the area of a rectange
+# I made it more complicated by having the user input the numbers
 print("\nPart 3:")
 width = int(input("What is the width? >> "))
 height = int(input("What is the height? >> "))
+
+# I then made it even more complicated by checking if the numbers were equal (making it a sqare)
+# Then changing what gets printed to show that you made a square
 if width == height:
     print(f"The area of your square is {width * height}")
 else:
     print(f"The area of your rectangle is {width * height}")
 
+
+# The fourth challenge was to convert temperature
+# To keep it consistant, i made the user input the temperature
 print("\nPart 4:")
 temp = int(input("What is the temperature? >> "))
+
+# Then to make it even more complicated, i had my program check if it is above -10 or 50 
+# Then assumed anything over 50 was Fahrinheight, anything over -10 was Celcius
+# And any anything else was an unreasonable temperature
 if temp >= 50:
     temp1 = temp - 32
     temp1 = temp1 * 0.5556
@@ -49,9 +67,11 @@ elif temp >= -10:
 else:
     print("This is not a valid temperature...")
 
+# The fifth challenge is all done in a function
 print("\nPart 5:")
 rand()
 
+# Then there were some extras where you messed around with different list types
 fruits_list = ["Banana", "Apple", "Kiwifruit", "Pineapple"]
 colours_tuple = ("Yellow", "Red", "Green", "Purple")
 food_dict = {"Pineapple": "Fruit", "Cake": "Sweet", "Carrot": "Vegetable"}
