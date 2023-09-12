213ae91f28c96427ed602d68967cd70153ced6c5
DanielDanielDanielandmoreDaniel <138410451+DanielDanielDanielandmoreDaniel@users.noreply.github.com>
2023-09-12 13:58:06 +1200

    Update TurtleCode.py
    Added more comments

--- TurtleCode.py
+++ TurtleCode.py
@@ -14,10 +14,12 @@ class Shape:
 # Child/Low level
 # Each of these functions have their own job
 class Star(Shape):
+    # This defines the colour and side length of the star shape
     def __init__(self, color, side_length):
         super().__init__(color)
         self.side_length = side_length
 
+    # This defines the actual shape
     def draw(self):
         for i in range(5):
             self.John.forward(self.side_length)
@@ -25,16 +27,19 @@ class Star(Shape):
 
 
 class BigStar(Shape):
+    # This defines the colour and side length of the star shape
     def __init__(self, color, side_length):
         super().__init__(color)
         self.side_length = side_length
 
+    # This defines the actual shape
     def draw(self):
         for i in range(15):
             self.John.forward(self.side_length)
             self.John.left(168)
 
 
+# This tells the classes what to do (Draw the big star, make it blue, and every line is 200px)
 Shape = BigStar("blue", 200)
 Shape.draw()
 
