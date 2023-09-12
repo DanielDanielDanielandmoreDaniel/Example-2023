39a3ddaddf7671ff91b48af492f54aff91b7b6ff
DanielDanielDanielandmoreDaniel <138410451+DanielDanielDanielandmoreDaniel@users.noreply.github.com>
2023-09-12 14:35:55 +1200

    Update OOP.py
    Comments, yay

--- OOP.py
+++ OOP.py
@@ -1,10 +1,13 @@
 class Car:
     def __init__(self, color, model, year, make):
+        # This is what defines the car
         self.color = color
         self.model = model
         self.year = year
         self.make = make
 
+    # These methods print out a statement that tells the user what the car is doing
+    # And which car is doing it
     def acc(self):
         return self.color, self.make, "is speeding up"
 
@@ -19,12 +22,15 @@ class Car:
 
 
 class SuperCar(Car):
+    # This child class does everything the other class does, but when it goes forward, it does it very quickly
     def sports_mode(self):
         return self.color, "is speeding up VERY QUICKLY AAAAAAAAAAAAAAAAAAAH"
 
 
+# Simply asking the user if they want a organge car, red car, or a super car
 car = input("What car do you want? >> ")
 
+# This creates the car and gives the class the attributes of the cars
 if car == "1":
     Inst1 = Car(color="Orange", model="Ford", year="2022", make="Mustang")
 elif car == "lrlrududaabbstart":
@@ -32,9 +38,11 @@ elif car == "lrlrududaabbstart":
 else:
     Inst1 = Car(color="Red", model="Toyota", year="2013", make="Corolla")
 
+# This starts the car
 ans = input("\nDo you want to start the car? >> ")
 print(Inst1.acc())
 
+# This loops until the user stops, and it is simply everything the car is programmed to do
 while ans != "stop":
     ans = input("\nWhat next? >> ")
     if ans == "left":
